03989f98c1cb4485d6d44ecd118d4363c48e9615
kishorek0344 <kishorekishk44@gmail.com>
2024-08-18 18:11:40 +0530

    domain url backend in index.js common

--- project-frontend-main/src/helpers/fetchCategoryWiseProduct.js
+++ project-frontend-main/src/helpers/fetchCategoryWiseProduct.js
@@ -3,8 +3,10 @@ const { default: SummaryApi } = require("../common")
 const fetchCategoryWiseProduct = async(category)=>{
     const response = await fetch(SummaryApi.categoryWiseProduct.url,{
         method : SummaryApi.categoryWiseProduct.method,
+        credentials: 'include',
         headers : {
-            "content-type" : "application/json"
+            "content-type" : "application/json",
+            
         },
         body : JSON.stringify({
             category : category
