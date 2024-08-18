53c8456cbe3ffafd3f5579da6a506497b270c4a7
kishorek0344 <kishorekishk44@gmail.com>
2024-08-18 18:02:24 +0530

    domain url backend in index.js common

--- project-backend-main/middleware/authToken.js
+++ project-backend-main/middleware/authToken.js
@@ -40,14 +40,18 @@
 // module.exports = authToken;
 
 
-const TOKEN_SECRET_KEY = "QWESDSDFFGGHTT^GYUAYUHHJDIJOSHUSDY&SUGSBJSSKLNDNBBSKHUHJUSIJLSIJIJIUBHJSDDS";
 const jwt = require("jsonwebtoken");
 require('dotenv').config();
+
+// Use the environment variable for the secret key if defined
+const TOKEN_SECRET_KEY = process.env.TOKEN_SECRET_KEY || "QWESDSDFFGGHTT^GYUAYUHHJDIJOSHUSDY&SUGSBJSSKLNDNBBSKHUHJUSIJLSIJIJIUBHJSDDS";
+
 async function authToken(req, res, next) {
   try {
     const token = req.cookies?.token;
 
-    console.log("token", token);
+    console.log("Token from cookies:", token);
+
     if (!token) {
       return res.status(401).json({
         message: "Please Login...!",
@@ -57,11 +61,8 @@ async function authToken(req, res, next) {
     }
 
     jwt.verify(token, TOKEN_SECRET_KEY, function (err, decoded) {
-      console.log(err);
-      console.log("decoded", decoded);
-
       if (err) {
-        console.log("error auth", err);
+        console.error("Error during token verification:", err);
         return res.status(401).json({
           message: "Unauthorized",
           error: true,
@@ -69,10 +70,12 @@ async function authToken(req, res, next) {
         });
       }
 
+      console.log("Decoded token:", decoded);
       req.userId = decoded?._id;
       next();
     });
   } catch (err) {
+    console.error("Error in authToken middleware:", err);
     res.status(400).json({
       message: err.message || err,
       data: [],
@@ -83,3 +86,4 @@ async function authToken(req, res, next) {
 }
 
 module.exports = authToken;
+
