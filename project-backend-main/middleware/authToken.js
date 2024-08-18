7d4d7c8d9d05afb929328e50c1044778adaf48f4
kishorek0344 <kishorekishk44@gmail.com>
2024-08-18 19:27:02 +0530

    domain url backend in index.js common

--- project-backend-main/middleware/authToken.js
+++ project-backend-main/middleware/authToken.js
@@ -1,89 +1,49 @@
-// const TOKEN_SECRET_KEY="QWESDSDFFGGHTT^GYUAYUHHJDIJOSHUSDY&SUGSBJSSKLNDNBBSKHUHJUSIJLSIJIJIUBHJSDDS"
+const jwt = require('jsonwebtoken')
 
-// const jwt = require("jsonwebtoken");
+async function authToken(req,res,next){
+    try{
+        const token = req.cookies?.token
 
-// async function authToken(req, res, next) {
-//   try {
-//     const token = req.cookies?.token;
+        console.log("token",token)
+        if(!token){
+            return res.status(200).json({
+                message : "Please Login...!",
+                error : true,
+                success : false
+            })
+        }
 
-//     console.log("token", token);
-//     if (!token) {
-//       return res.status(200).json({
-//         message: "Please Login...!",
-//         error: true,
-//         success: false,
-//       });
-//     }
+        jwt.verify(token, process.env.TOKEN_SECRET_KEY, function(err, decoded) {
+            console.log(err)
+            console.log("decoded",decoded)
+            
+            if(err){
+                console.log("error auth", err)
+            }
 
-//     jwt.verify(token, process.env.TOKEN_SECRET_KEY, function (err, decoded) {
-//       console.log(err);
-//       console.log("decoded", decoded);
+            req.userId = decoded?._id
 
-//       if (err) {
-//         console.log("error auth", err);
-//       }
-
-//       req.userId = decoded?._id;
-
-//       next();
-//     });
-//   } catch (err) {
-//     res.status(400).json({
-//       message: err.message || err,
-//       data: [],
-//       error: true,
-//       success: false,
-//     });
-//   }
-// }
-
-// module.exports = authToken;
-
-
-const jwt = require("jsonwebtoken");
-require('dotenv').config();
-
-// Use the environment variable for the secret key if defined
-const TOKEN_SECRET_KEY = process.env.TOKEN_SECRET_KEY ||"QWESDSDFFGGHTT^GYUAYUHHJDIJOSHUSDY&SUGSBJSSKLNDNBBSKHUHJUSIJLSIJIJIUBHJSDDS";
-
-async function authToken(req, res, next) {
-  try {
-    const token = req.cookies?.token;
-
-    console.log("Token from cookies:", token);
-
-    if (!token) {
-      return res.status(401).json({
-        message: "Please Login...!",
-        error: true,
-        success: false,
-      });
-    }
-
-    jwt.verify(token, TOKEN_SECRET_KEY, function (err, decoded) {
-      if (err) {
-        console.error("Error during token verification:", err);
-        return res.status(401).json({
-          message: "Unauthorized",
-          error: true,
-          success: false,
+            next()
         });
-      }
 
-      console.log("Decoded token:", decoded);
-      req.userId = decoded?._id;
-      next();
-    });
-  } catch (err) {
-    console.error("Error in authToken middleware:", err);
-    res.status(400).json({
-      message: err.message || err,
-      data: [],
-      error: true,
-      success: false,
-    });
-  }
+
+    }catch(err){
+        res.status(400).json({
+            message : err.message || err,
+            data : [],
+            error : true,
+            success : false
+        })
+    }
 }
 
-module.exports = authToken;
+
+module.exports = authToken
+
+
+
+
+
+
+
 
--- project-frontend-main/src/helpers/addToCart.js
+++ project-frontend-main/src/helpers/addToCart.js
@@ -1,43 +1,35 @@
-import SummaryApi from "../common";
-import { toast } from 'react-toastify';
-
+import SummaryApi from "../common"
+import { toast } from 'react-toastify'
 
 const addToCart = async (e, id) => {
-    e?.stopPropagation();
-    e?.preventDefault();
+    e?.stopPropagation()
+    e?.preventDefault()
 
-    try {
-        const response = await fetch(SummaryApi.addToCartProduct.url, {
-            method: SummaryApi.addToCartProduct.method,
-            credentials: 'include', // This ensures cookies are sent with the request
-            headers: {
-                "Content-Type": 'application/json',
-                'Authorization': 'Bearer ${"QWESDSDFFGGHTT^GYUAYUHHJDIJOSHUSDY&SUGSBJSSKLNDNBBSKHUHJUSIJLSIJIJIUBHJSDDS"}'
-                
-            },
-            body: JSON.stringify({ productId: id }),
-          
-        });
+    const response = await fetch(SummaryApi.addToCartProduct.url, {
+        method: SummaryApi.addToCartProduct.method,
+        credentials: 'include',
+        headers: {
+            "content-type": 'application/json'
+        },
+        body: JSON.stringify(
+            { productId: id }
+        )
+    })
 
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
+    const responseData = await response.json()
 
-        const responseData = await response.json();
-
-        if (responseData.success) {
-            toast.success(responseData.message);
-        } else if (responseData.error) {
-            toast.error(responseData.message);
-        }
-
-        return responseData;
-
-    } catch (error) {
-        console.error('Error in addToCart:', error);
-        toast.error('An error occurred while adding to cart.');
+    if (responseData.success) {
+        toast.success(responseData.message)
     }
-};
+
+    if (responseData.error) {
+        toast.error(responseData.message)
+    }
 
 
-export default addToCart;
+    return responseData
+
+}
+
+
+export default addToCart
